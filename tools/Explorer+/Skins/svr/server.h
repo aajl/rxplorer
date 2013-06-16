a7665729eb9b6937064680b6a1a242405e3b2edb
smartmyth <smartmyth@gmail.com>
2013-06-16 15:59:21 +0000

    通用服务器类.

---
+++ tools/Explorer+/Skins/svr/server.h
@@ -0,0 +1,119 @@
+#pragma once
+
+#include "session.h"
+#include "client.h"
+
+#include <boost/thread.hpp>
+
+namespace gtl
+{
+
+namespace svr
+{
+
+class server
+{
+public:
+	server(boost::asio::io_service& io_service, short port)
+		: io_service_(io_service)
+		, acceptor_(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port))
+	{
+		boost::shared_ptr<session> new_session(new session(io_service_));
+		acceptor_.async_accept(new_session->socket(), boost::bind(
+			&server::handle_accept, this, new_session,
+			boost::asio::placeholders::error));
+	}
+
+	void handle_accept(boost::shared_ptr<session> new_session, const boost::system::error_code& error)
+	{
+		if (!error)
+		{
+			TRACE("new session\n");
+			new_session->start();
+
+			// 连接成功,保存当前会话.
+			gtl::tstr ip(new_session->socket().remote_endpoint().address().to_string());
+			boost::shared_ptr<client> clnt(new client(this, ip, _T(""), new_session));
+			new_session->set_client(clnt.get());
+			m_sessions.push_back(clnt);
+
+			new_session.reset(new session(io_service_));
+			acceptor_.async_accept(new_session->socket(), boost::bind(
+				&server::handle_accept, this, new_session,
+				boost::asio::placeholders::error));
+		}
+	}
+
+	void run()
+	{
+		io_thread_.reset(new boost::thread(boost::bind(&boost::asio::io_service::run, &io_service_)));
+	}
+
+	void stop()
+	{
+		io_service_.stop();
+		io_thread_->join();
+	}
+
+public:
+	void handle_login(boost::shared_ptr<client> clnt, const gtl::tstr& name)
+	{
+		const gtl::tstr& ip = clnt->ip();
+		boost::shared_ptr<session> sess = clnt->sess();
+
+		auto first = m_clients.begin();
+		auto last = m_clients.end();
+		for(; first != last; ++first)
+		{
+			boost::shared_ptr<client> cln = *first;
+			if((cln->ip() + cln->name()) == (ip + name))
+			{
+				cln->set_session(sess);
+				sess->set_client(cln.get());
+				remove_session(cln);
+
+				TRACE("relog, client count: %d\n", m_clients.size());
+				return;
+			}
+		}
+
+		boost::shared_ptr<client> new_clnt(new client(this, ip, name, sess));
+		sess->set_client(clnt.get());
+
+		m_clients.push_back(new_clnt);
+		TRACE("client count: %d\n", m_clients.size());
+	}
+
+public:
+	const std::list<boost::shared_ptr<client>>& clients()
+	{
+		return m_clients;
+	}
+
+protected:
+	void remove_session(boost::shared_ptr<client> clnt)
+	{
+		auto first = m_sessions.begin();
+		auto last = m_sessions.end();
+		for(; first != last; ++first)
+		{
+			if(clnt.get() == (*first).get())
+			{
+				m_sessions.erase(first);
+				break;
+			}
+		}
+	}
+
+private:
+	boost::shared_ptr<boost::thread> io_thread_;
+	boost::asio::io_service& io_service_;
+	boost::asio::ip::tcp::acceptor acceptor_;
+
+	std::list<boost::shared_ptr<client>> m_sessions;
+	std::list<boost::shared_ptr<client>> m_clients;
+};
+
+}
+
+}
---
+++ tools/Explorer+/Skins/svr/session.h
@@ -0,0 +1,103 @@
+#pragma once
+
+#include "client.h"
+
+#include <boost/bind.hpp>
+#include <boost/thread.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
+namespace gtl
+{
+
+namespace svr
+{
+
+class session
+	: public boost::enable_shared_from_this<session>
+{
+public:
+	session(boost::asio::io_service& io_service)
+		: m_socket(io_service)
+		, m_client(NULL)
+	{
+	}
+
+	boost::asio::ip::tcp::socket& socket()
+	{
+		return m_socket;
+	}
+
+	void set_client(client* clnt)
+	{
+		m_client = clnt;
+	}
+
+	void start()
+	{
+		m_socket.async_read_some(boost::asio::buffer(m_recv_buff, max_length),
+			boost::bind(&session::handle_read, shared_from_this(),
+			boost::asio::placeholders::error,
+			boost::asio::placeholders::bytes_transferred));
+	}
+
+	void handle_read(const boost::system::error_code& error, size_t bytes_transferred)
+	{
+		if (!error)
+		{
+			m_recv_buff[bytes_transferred] = 0;
+			TRACE("recv: %s\n", m_recv_buff);
+
+			if(m_client != NULL)
+				m_client->handle_recv(m_recv_buff, bytes_transferred);
+
+			m_socket.async_read_some(boost::asio::buffer(m_recv_buff, max_length),
+				boost::bind(&session::handle_read, shared_from_this(),
+				boost::asio::placeholders::error,
+				boost::asio::placeholders::bytes_transferred));
+		}
+		else
+		{
+			if(m_client != NULL)
+				m_client->handle_close();
+
+			TRACE("read closed\n");
+		}
+	}
+
+	void handle_write(const boost::system::error_code& error)
+	{
+		if (!error)
+		{
+			TRACE("send completed\n");
+			if(m_client != NULL)
+				m_client->handle_write();
+		}
+		else
+		{
+			TRACE("write closed\n");
+			if(m_client != NULL)
+				m_client->handle_close();
+		}
+	}
+
+public:
+	void send(const gtl::str& data)
+	{
+		boost::asio::async_write(m_socket, boost::asio::buffer(data.c_str(), data.capacity()),
+			boost::bind(&session::handle_write, this, boost::asio::placeholders::error));
+	}
+
+private:
+	enum
+	{
+		max_length = 1024
+	};
+
+	client* m_client;
+	char m_recv_buff[max_length];
+	boost::asio::ip::tcp::socket m_socket;
+};
+
+} // end of namespace svr
+
+} // end of namespace gtl
