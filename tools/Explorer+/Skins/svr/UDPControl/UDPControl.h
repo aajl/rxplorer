5fb44c6697536a68e67fb471e6f0477b8ff91f94
smartmyth <smartmyth@gmail.com>
2013-07-11 07:57:45 +0000

    增加修改后的.

---
+++ tools/Explorer+/Skins/svr/UDPControl/UDPControl.h
@@ -0,0 +1,32 @@
+
+// UDPControl.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CUDPControlApp:
+// See UDPControl.cpp for the implementation of this class
+//
+
+class CUDPControlApp : public CWinApp
+{
+public:
+	CUDPControlApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CUDPControlApp theApp;
---
+++ tools/Explorer+/Skins/svr/UDPControl/UDPControlDlg.h
@@ -0,0 +1,67 @@
+
+// UDPControlDlg.h : header file
+//
+
+#pragma once
+
+#include <gtl/net/socket.h>
+#include <gtl/xml/xml.h>
+#include <gtl/thread/thread.h>
+#include <gtl/thread/callback.h>
+#include <gtl/mpl/fun.h>
+
+// CUDPControlDlg dialog
+class CUDPControlDlg : public CDialogEx
+{
+// Construction
+public:
+	CUDPControlDlg(CWnd* pParent = NULL);	// standard constructor
+	virtual ~CUDPControlDlg();
+
+// Dialog Data
+	enum { IDD = IDD_UDPCONTROL_DIALOG };
+	enum { Timer_Poweroff, Timer_Reboot };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+public:
+	bool Poweroff(const gtl::str& cmd);
+	bool Reboot(const gtl::str& cmd);	
+	bool CancelPoweroff(const gtl::str& cmd);
+	bool CancelReboot(const gtl::str& cmd);
+	bool Cmd(const gtl::str& cmd);
+	bool Shortcut(const gtl::str& cmd);
+
+// Implementation
+protected:
+	HICON m_hIcon;
+	void SaveSetting();
+	void Start();
+	void Stop();
+
+	BOOL SysPoweroff();
+	BOOL SysReboot();
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedChksavelog();
+	afx_msg void OnBnClickedBtnstart();
+	CString m_strRemoteIP;
+	int m_nRemotePort;
+	int m_nLocalPort;
+	BOOL m_bSaveLog;
+	CString m_strLog;
+	virtual BOOL PreTranslateMessage(MSG* pMsg);
+	CString m_strLocalIP;
+	gtl::xml m_xml;
+	gtl::net::socket m_udp;
+	gtl::thread<> m_thread;
+	gtl::callback<gtl::mpl::fun<void (const gtl::str& /*cmd*/, 	bool (CUDPControlDlg::*)(const gtl::str&), const gtl::tstr& /*log*/)>> m_callback;
+	bool m_start;
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+};
