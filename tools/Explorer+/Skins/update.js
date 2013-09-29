7de00478d753ec29aeb67b081d5253a77fe6b698
smartmyth <smartmyth@gmail.com>
2013-09-29 13:40:46 +0000

    个性关于界面.

--- tools/Explorer+/Skins/update.js
+++ tools/Explorer+/Skins/update.js
@@ -1,11 +1,13 @@
 function auto_update() {
-	if(sys.updater.downloaded())
-	{
-		sys.shell_execute(sys.path.app + "/Explorer+.exe",  "-app -mutex explorer_plus_update_mutex -Module Modules/MUpdate.dll");
-		return "exit";
+	var downloaded = Number(sys.call('update.downloaded', 'explorer'));
+	if(downloaded) {
+		print("........................................\n");
+		//sys.shell_execute(sys.path.app + "/explorer+.exe",  "-app -mutex auto_update_mutex -dui Modules/MDirectUI.dll -Module Modules/MUpdate.dll unused -skin update/skins/skin.xml -appname explorer");
+
+		//return "exit";
 	}
 	
 	return "";
-}
+};
 
 auto_update();
