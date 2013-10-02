2a78358d23f3fbf91e8dad6d4fa71d306ceb12b0
smartmyth@gmail.com <smartmyth@gmail.com@25b2e33e-bf07-11de-a8ef-132414f8baa6>
2013-10-02 09:11:16 +0000

    修改界面.

--- trunk/tools/Explorer+/Skins/update.js
+++ trunk/tools/Explorer+/Skins/update.js
@@ -1,10 +1,9 @@
 function auto_update() {
 	var downloaded = Number(sys.call('update.downloaded', 'explorer'));
 	if(downloaded) {
-		print("........................................\n");
-		//sys.shell_execute(sys.path.app + "/explorer+.exe",  "-app -mutex auto_update_mutex -dui Modules/MDirectUI.dll -Module Modules/MUpdate.dll unused -skin update/skins/skin.xml -appname explorer");
-
-		//return "exit";
+		var param = "-wait explorer_plus_direct_ui_mutex -waittime 5000 -start app_d.exe -app -mutex auto_update_mutex -dui Modules/MDirectUI_D.dll -Module Modules/MUpdate_D.dll unused -skin update/skins/skin.xml -appname explorer -localver Skins/version.xml";
+		sys.shell_execute(sys.path.app + "/app_d.exe",  param);
+		return "exit";
 	}
 	
 	return "";
